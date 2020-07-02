9324654cc7319ad32f4e36ca95313dd96993bf13
Russi4nBe4r <mterek97@gmail.com>
2020-07-03 00:11:35 +0300

    Initial commit

---
+++ graphic/Resource.h
@@ -0,0 +1,29 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется graphic.rc
+//
+#define IDC_MYICON                      2
+#define IDD_GRAPHIC_DIALOG              102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_GRAPHIC                     107
+#define IDI_SMALL                       108
+#define IDC_GRAPHIC                     109
+#define IDR_MAINFRAME                   128
+#define ID_SCALE                        777
+#define ID_NORMALSCALE                  778
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
---
+++ graphic/graphic.cpp
@@ -0,0 +1,241 @@
+﻿// graphic.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "graphic.h"
+#include "math.h"
+#include<string>
+#include<ctime>
+
+BOOL DLine(HDC hdc, double x1, double y1, double x2, double y2);
+#define MAX_LOADSTRING 100
+
+char szProgName[] = "Имя программы";
+
+int  xView, yView, test = -5, ff = 0, check = 0;;
+double i, r;
+double y, x;
+char Buf[100];
+#define pi 3.14
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// TODO: Разместите код здесь.
+
+	// Инициализация глобальных строк
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_GRAPHIC, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// Выполнить инициализацию приложения:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_GRAPHIC));
+
+	MSG msg;
+
+	// Цикл основного сообщения:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GRAPHIC));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_GRAPHIC);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		0, 0, 900, 900, nullptr, nullptr, hInstance, nullptr);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND  - обработать меню приложения
+//  WM_PAINT    - Отрисовка главного окна
+//  WM_DESTROY  - отправить сообщение о выходе и вернуться
+//
+//
+HPEN hPen;
+int dog = 0;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc; //создаём контекст устройства
+	PAINTSTRUCT ps; //создаём экземпляр структуры графического вывода
+	//HPEN hPen; //создаём перо
+	//Цикл обработки сообщений
+	switch (message)
+	{
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// Разобрать выбор в меню:
+		switch (wmId)
+		{
+		case ID_SCALE:
+			dog = 1;
+			SendMessage(hWnd, WM_PAINT, WPARAM(hWnd), NULL);
+			InvalidateRect(hWnd, NULL, TRUE);
+			break;
+		case ID_NORMALSCALE:
+			dog = 0;
+			SendMessage(hWnd, WM_PAINT, WPARAM(hWnd), NULL);
+			InvalidateRect(hWnd, NULL, TRUE);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	}
+	case WM_SIZE:
+		xView = LOWORD(lParam);
+		yView = HIWORD(lParam);
+
+		break;
+
+
+		//сообщение рисования
+
+	case WM_TIMER:
+		InvalidateRect(hWnd, NULL, TRUE);
+	case WM_CREATE:
+		SetTimer(hWnd, 1, 1000, NULL);
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		SetMapMode(hdc, MM_ISOTROPIC); //логические единицы отображаем, как физические
+		SetWindowExtEx(hdc, 900, 900, NULL); //Длина осей
+		SetViewportExtEx(hdc, xView, -yView, NULL); //Определяем облась вывода
+		SetViewportOrgEx(hdc, xView / 2, yView / 2, NULL); //Начало координат
+
+		//Рисуем оси координат
+		DLine(hdc, 0, -450, 0, 450);//ось У
+		DLine(hdc, -450, 0, 450, 0);//ось Х
+		if (ff == 0) {
+			MoveToEx(hdc, 0, 0, NULL);
+		}//перемещаемся в начало координат
+
+		//Создание красного пера
+		hPen = CreatePen(1, 4, RGB(255, 25, 0));
+		SelectObject(hdc, hPen);
+				for (i = 0; i <= 720	; i++) {
+					r = i * pi / 180;
+						srand(time(0));
+						hPen = CreatePen(1, 4, RGB(rand() % 255, 0, 0));
+						SelectObject(hdc, hPen);
+						//y = (1 + 2*sin(15*r))*(1 + cos(35*r)) ^ (1 + 5*cos(6*r))*(1 - sin(20*r));
+
+						y = (1 + 2 * sin(15 * r)) * pow((1 + cos(35 * r)), (1 + 5 * cos(6 * r))) * (1 - sin(20 * r));
+						x = i;
+
+						//LineTo(hdc, x, y);
+						if ((ff == 0)) {
+							MoveToEx(hdc, x, y, NULL);
+							ff = 1;
+						}
+						LineTo(hdc, x, y);
+
+					//MoveToEx(hdc, x, y, NULL);
+
+				//MoveToEx(hdc, count, y, NULL);
+
+				}
+		//InvalidateRect(hWnd, NULL, TRUE);
+		//ValidateRect(hWnd, NULL); //Обновляем экран
+		EndPaint(hWnd, &ps);
+		ff = 0;
+		check = 0;
+		test = -5;
+		break;
+
+		//сообщение выхода - разрушение окна
+	case WM_DESTROY:
+		DeleteObject(hPen); //не забываем уничтожать перья
+		PostQuitMessage(0); //Посылаем сообщение выхода с кодом 0 - нормальное завершение
+		break;
+
+	default:
+		return(DefWindowProc(hWnd, message, wParam, lParam)); //освобождаем очередь приложения от нераспознаных
+	}
+	return 0;
+}
+BOOL DLine(HDC hdc, double x1, double y1, double x2, double y2)
+{
+	MoveToEx(hdc, x1, y1, NULL); //сделать текущими координаты x1, y1
+	return LineTo(hdc, x2, y2); //нарисовать линию
+}
